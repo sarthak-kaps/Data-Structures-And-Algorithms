3d2e15135fee043f7e59b7687f2564eb370a1de4
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-01 22:15:36 +0530

    Add generic graph class

---
+++ Graph/graph.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* [Description] : Generic Graph class
+ *                 Not for regular use, prefer vector<vector<..>> in general
+ *                 This interface is implemented to unify implementation of various different graph algorithms 
+ */
+
+template <class T>
+class graph {
+public :
+    struct edge {
+        int from;
+        int to;
+        T cost;
+    };
+
+    vector<vector<edge>> g;
+    int n;
+    graph(int _n) : n(_n) {
+        g.resize(n);   
+    }
+    virtual void add(int u, int v, T cost);
+};
