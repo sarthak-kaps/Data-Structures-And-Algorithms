2a95e2003158e7fbfeb9369a592e3c11339d8de0
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-07 23:24:35 +0530

    Add support for reversing graph

--- Graph/digraph.cpp
+++ Graph/digraph.cpp
@@ -34,6 +34,15 @@ public :
     digraph(int _n) : graph<T>(_n) {}
 
     void add(int from, int to, T cost) {
+        assert(0 <= from and from < n and 0 <= to and to < n);
         g[from].push_back({from, to, cost});
+    }
+    
+    digraph<T> reverse() {
+        digraph<T> rev_graph(n);
+        for(auto& e : g) {
+            rev_graph.add(e.to, e.from, e.cost);
+        }
+        return rev_graph;
     }
 };
