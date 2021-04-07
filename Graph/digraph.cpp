cc9a529a450d1450ca19139eac2089e8755e8e24
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-07 22:46:31 +0530

    Added directed graph

---
+++ Graph/digraph.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* [Description] : Generic Directed Graph class
+ *                 Not for regular use, prefer vector<vector<..>> in general
+ *                 This interface is implemented to unify implementation of various different graph algorithms 
+ */
+
+template <class T>
+class graph {
+public :
+    struct edge {
+        int from;
+        int to;
+        T cost;
+    };
+
+    vector<vector<edge>> g;
+    int n;
+    
+    graph(int _n) : n(_n) {
+        g.resize(n);   
+    }
+    virtual void add(int u, int v, T cost);
+};
+
+template <class T>
+class digraph : public graph<T> {
+public :
+    using graph<T>::g;
+    using graph<T>::n;
+
+    digraph(int _n) : graph<T>(_n) {}
+
+    void add(int from, int to, T cost) {
+        g[from].push_back({from, to, cost});
+    }
+};
